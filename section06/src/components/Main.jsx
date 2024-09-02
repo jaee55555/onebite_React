6e7d5c53832a3195f8b5fdddd5f4f5bfcca2533c
jaee55555 <jaeeO55555@gmail.com>
2024-09-02 22:16:51 +0900

    how to use props

--- section06/src/components/Main.jsx
+++ section06/src/components/Main.jsx
@@ -1,9 +1,28 @@
+import "./Main.css"
+
+//JSX 주의사항
+//1. 중괄호{} 안에는 자바스크립트 표현식만 넣을 수 있다(조건문, 반복문 사용불가)
+//2. 숫자, 문자열, 배열 값만 정상적으로 랜더링
+//3. 모든 태그는 닫혀있어야 한다.
+//4. 최상위 태그는 반드시 하나여야만 한다.
+
 const Main = () =>{
-    return(
-        <main>
-            <h1>main</h1>
-        </main>
-    );
+    const user = {
+        name: '오윤재',
+        isLogin: true,
+    };
+
+    if (user.isLogin){
+        return <div className="logout">로그아웃</div>;
+    }
+    else{
+        return <div>로그인</div>;
+    }
+    // return(
+    //     <>
+    //     {user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}
+    //     </>
+    // );
 };
 
 export default Main;
