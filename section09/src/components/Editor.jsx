6cb02ec611d7393a2561fa552cd5c40cb9807a7f
jaee55555 <jaeeO55555@gmail.com>
2024-09-03 04:36:31 +0900

    Todo List project

--- section09/src/components/Editor.jsx
+++ section09/src/components/Editor.jsx
@@ -1,10 +1,41 @@
-import "./Editor.css"
+import { useRef, useState } from "react";
+import "./Editor.css";
 
-const Editor = () =>{
-    return <div className="Editor">
-        <input placeholder="새로운 Todo..."/>
-        <button>추가</button>
-    </div>;
-}
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={inputRef}
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
 
 export default Editor;
--- section09/src/components/List.jsx
+++ section09/src/components/List.jsx
@@ -1,17 +1,49 @@
-import "./List.css"
+import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () =>{
-    return <div className="List">
-        <h4>Todo List 🌱</h4>
-        <input placeholder="검색어를 입력하세요" />
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
 
-        <div className="todos_wrapper">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-        </div>
-    </div>;
-}
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default List;
