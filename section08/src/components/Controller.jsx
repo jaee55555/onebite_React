b504b6268a84e05ec850ee7e1cc36e0dac20b505
jaee55555 <jaeeO55555@gmail.com>
2024-09-03 03:01:49 +0900

    useEffect and lifecycle

--- section08/src/components/Controller.jsx
+++ section08/src/components/Controller.jsx
@@ -2,7 +2,7 @@ const Controller = ({onClickButton}) =>{
     return(
         <div>
             <button onClick={()=>{onClickButton(-1);}}>-1</button>
-            <button onClick={()=>{onClickButton(-1);}}>-10</button>
+            <button onClick={()=>{onClickButton(-10);}}>-10</button>
             <button onClick={()=>{onClickButton(-100);}}>-100</button>
             <button onClick={()=>{onClickButton(100);}}>+100</button>
             <button onClick={()=>{onClickButton(10);}}>+10</button>
