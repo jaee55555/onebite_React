060ace248a82bc4a35dc8169f2e3ed5427a9350f
jaee55555 <jaeeO55555@gmail.com>
2024-09-03 02:57:09 +0900

    useEffect and lifecycle

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,15 +1,34 @@
 import './App.css'
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
-import { useState,useEffect } from 'react';
+import { useState,useEffect, useRef } from 'react';
+import Even from './components/Even';
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
+  const isMount = useRef(false);
+
+  //1. 마운트: 탄생
   useEffect(()=>{
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
+    console.log("mount");
+  }, []);
+
+  //2. 업데이트: 변화, 리렌더링
+  useEffect(()=>{
+    if(!isMount.current){
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  //3. 언마운트: 죽음
+
+  // useEffect(()=>{
+  //   console.log(`count: ${count} / input: ${input}`);
+  // }, [count, input]);
   //의존성 배열
   //dependency array
   //deps
@@ -29,6 +48,7 @@ function App() {
     
     <section>
       <Viewer count={count}/>
+      {count % 2 === 0 ? <Even/> : null}
     </section>
     
     <section>
